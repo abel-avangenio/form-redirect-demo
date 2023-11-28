47ee48d2e46dd7a8f711d2ed99793b8f27fa966a
abel-avangenio <abel.rt@avangenio.com>
2023-11-27 19:36:15 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-card>
+    <v-card-title>Form name goes here</v-card-title>
+    <v-card-subtitle>Form subtitle goes here</v-card-subtitle>
+    <v-card-text>Form body goes here</v-card-text>
+    <v-card-actions>
+      <v-btn>Submit</v-btn>
+      <v-btn>Cancel</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
